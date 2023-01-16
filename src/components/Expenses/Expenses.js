3f2eee383a0d84572d3cf982282684a5709444f4
FAHED-OMEN\fahed <falsmadi.c@stc.com.sa>
2023-01-16 08:35:49 +0300

    added components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,21 @@
 import './Expenses.css'
 import Card from '../UI/Card';
-import ExpenseItem from "./ExpenseItem";
 import React,{useState} from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
     const [dateFilter, setDateFilter] = useState('2020');
     const onChangeDateFilterHandler = (dateChanged) => {
         setDateFilter(dateChanged);
-        props.onChangeDateFilter(dateChanged);
     };
     const filteredExpenses = props.expenses.filter((expense) => expense.date.getFullYear().toString() === dateFilter);
 
     return (
             <Card className="expenses">
                 <ExpensesFilter selected={dateFilter} onChangeDateFilter={onChangeDateFilterHandler} />
+                <ExpensesChart expenses={filteredExpenses}/>
                 <ExpensesList items={filteredExpenses} />
             </Card>
     );
